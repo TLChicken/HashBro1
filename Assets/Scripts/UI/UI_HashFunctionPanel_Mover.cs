18428ee0322b1590909450635bf4eed201428fe8
TLChicken <tehlightningchicken@gmail.com>
2021-06-16 21:22:45 +0800

    Created script UI_HashFunctionPanel_Mover for showing and hiding the different UIs.

---
+++ Assets/Scripts/UI/UI_HashFunctionPanel_Mover.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_HashFunctionPanel_Mover : MonoBehaviour
+{
+
+    public GameObject openedMarker;
+    public GameObject closedMarker;
+    private RectTransform rt;
+    private Vector2 openPosition;
+
+    private Vector2 closedPosition;
+    private bool currOpen;
+    void Start () {
+        rt = GetComponent<RectTransform>();
+        if (openedMarker == null) {
+            openPosition = rt.transform.localPosition;
+        } else {
+            openPosition = openedMarker.GetComponent<Transform>().localPosition;
+        }
+        
+        closedPosition = closedMarker.GetComponent<Transform>().localPosition;
+        Debug.Log(closedMarker.GetComponent<Transform>().localPosition);
+        Debug.Log(closedMarker.GetComponent<Transform>().position);
+        currOpen = true;
+        
+    }
+    public void Toggle () {
+        if (currOpen)
+        {
+            rt.anchoredPosition = closedPosition;
+            currOpen = false;
+        }
+        else
+        {
+            rt.anchoredPosition = openPosition;
+            currOpen = true;
+        }
+        
+    }
+}
