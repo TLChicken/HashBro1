0bd7b1595484e77ddc0d8259c9c5a55376cecb11
TLChicken <tehlightningchicken@gmail.com>
2021-06-27 03:56:20 +0800

    Created Scene Template 2 and Level TLC1

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour {
     // loads the next scene in the queue
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
 
     public void QuitGame() {
         Debug.Log("Quit!");
