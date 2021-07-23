65eb6cee8840430719cc777b3427f7606cfc2e9f
TLChicken <tehlightningchicken@gmail.com>
2021-07-24 00:16:15 +0800

    Add Gate (Model, Open and Close Animations)

--- Assets/Scripts/Puzzle/PuzzleController.cs
+++ Assets/Scripts/Puzzle/PuzzleController.cs
@@ -6,15 +6,20 @@ public class PuzzleController : MonoBehaviour {
 
 
     public Color puzzleColor;
-    public List<GameObject> currPuzzlePieces;
+    [SerializeField]
+    [Tooltip("Assign the puzzle pieces to be solved for this puzzle to complete")]
+    //This list is only for attaching the puzzle pieces in the Inspector. The actual variable that contains the puzzle pieces is the next one.
+    private List<GameObject> currPuzzlePiecesTEMP;
+    public List<PuzzlePieceInterface> currPuzzlePieces = new List<PuzzlePieceInterface>();
 
     // Start is called before the first frame update
     void Start() {
-        foreach (GameObject currObj in currPuzzlePieces) {
+        foreach (GameObject currObj in currPuzzlePiecesTEMP) {
             PuzzlePieceInterface currPiece = currObj.GetComponent<PuzzlePieceInterface>();
             if (currPiece == null) {
                 continue;
             }
+            currPuzzlePieces.Add(currPiece);
 
             currPiece.changeColor(puzzleColor);
         }
@@ -25,11 +30,7 @@ public class PuzzleController : MonoBehaviour {
 
     public bool isCurrPuzzleSolved() {
         bool solved = true;
-        foreach (GameObject currObj in currPuzzlePieces) {
-            PuzzlePieceInterface currPiece = currObj.GetComponent<PuzzlePieceInterface>();
-            if (currPiece == null) {
-                continue;
-            }
+        foreach (PuzzlePieceInterface currPiece in currPuzzlePieces) {
 
             solved = solved && currPiece.isCurrentlyCorrect();
         }
